cfe469af2a7caa9904eb199a3766e754607ddd5e
Shelbot77 <alessiodeangelis021@gmail.com>
2023-01-15 12:33:38 +0100

    Creazione scaffolding + importazione file.vue e store.js

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+import { store } from '../store'
+export default {
+    name: 'AppHeader',
+    //components
+    data() {
+        return {
+            store,
+        }
+    }
+
+}
+</script>
+<template >
+    <h1>Questo è l'header</h1>
+
+</template>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+import { store } from '../store'
+export default {
+    name: 'AppMain',
+    //components
+    data() {
+        return {
+            store,
+        }
+    }
+
+}
+</script>
+<template >
+    <h2>Questo è il main</h2>
+
+</template>
+<style lang="scss" scoped>
+
+</style>
